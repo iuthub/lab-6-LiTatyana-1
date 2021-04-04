dab4633a465533bffe9a4260269ebf68d8a82fba
LiTatyana <72077299+LiTatyana@users.noreply.github.com>
2021-04-04 19:31:38 +0500

    Add files via upload
    1st program is completed

--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,57 +1,66 @@
 <?php
 
-	$pattern="";
-	$text="";
-	$replaceText="";
-	$replacedText="";
-
-	$match="Not checked yet.";
+$pattern="";
+$text="";
+$replaceText="";
+$replacedText="";
+$containString = "/\b($text)\b/";
+$containEmail = "/\b([A-Za-z0-9._%+-]{2,})+@([A-Za-z0-9._]{2,})+\.[A-Za-z]{2,6}\b/";
+$containPhone = "/^(\+(998))+\-([0-9]{2})+\-([0-9]{3})+\-([0-9]{4})/";
+$match="Not checked yet.";
 
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
-	$pattern=$_POST["pattern"];
-	$text=$_POST["text"];
-	$replaceText=$_POST["replaceText"];
+    $pattern=$_POST["pattern"];
+    $text=$_POST["text"];
+    $replaceText=$_POST["replaceText"];
 
-	$replacedText=preg_replace($pattern, $replaceText, $text);
+    $replacedText=preg_replace($pattern, $replaceText, $text);
 
-	if(preg_match($pattern, $text)) {
-						$match="Match!";
-					} else {
-						$match="Does not match!";
-					}
+    if(preg_match($pattern, $text)) {
+        $match="Match!";
+    } else {
+        $match="Does not match!";
+    }
 }
-
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Valid Form</title>
+    <meta charset="UTF-8">
+    <title>Valid Form</title>
 </head>
 <body>
-	<form action="regex_valid_form.php" method="post">
-		<dl>
-			<dt>Pattern</dt>
-			<dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
+<h1>Validation Tests</h1>
+<h3>Choose Pattern:</h3>
+<select name=patOption>
+    <option>OPTIONS...</option>
+    <option value=<?= $containString ?>>Contain_String</option>
+    <option value=<?= $containEmail ?>>Contain_Email</option>
+    <option value=<?= $containPhone ?>>Contain_Phone</option>
+</select>
+<form action="regex_valid_form.php" method="post">
+    <dl>
+        <dt>Pattern</dt>
+        <dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
 
-			<dt>Text</dt>
-			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
+        <dt>Text</dt>
+        <dd><input type="text" name="text" value="<?= $text ?>"></dd>
 
-			<dt>Replace Text</dt>
-			<dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
+        <dt>Replace Text</dt>
+        <dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
 
-			<dt>Output Text</dt>
-			<dd><?=	$match ?></dd>
+        <dt>Output Text</dt>
+        <dd><?=	$match ?></dd>
 
-			<dt>Replaced Text</dt>
-			<dd> <code><?=	$replacedText ?></code></dd>
+        <dt>Replaced Text</dt>
+        <dd> <code><?=	$replacedText ?></code></dd>
 
-			<dt>&nbsp;</dt>
-			<dd><input type="submit" value="Check"></dd>
-		</dl>
+        <dt>&nbsp</dt>
+        <dd><input type="submit" value="Check"></dd>
+    </dl>
 
-	</form>
+</form>
 </body>
 </html>
